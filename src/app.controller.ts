274ee521dc907e97ec7da8d2cefb06fed35515be
thomegabi <gabi10thome@hotmail.com>
2024-04-30 22:30:04 -0300

    Alterações com integração ao Prisma

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { PrismaService } from './prisma.service';
 
-@Controller()
+@Controller("notifications") // Se for colocado algo dentro desse decorator como "app", vai ser necessário expecificar na URL final, nesse caso seris "localhost:3000/app" 
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get() // Aqui segue a mesma logica, como o get esta dentro do Controller, para chegar na pag agora sera necessário digitar "localhost:3000/app/test"
+  getHello() {
+    return this.prisma.notification.findMany();
   }
 }
