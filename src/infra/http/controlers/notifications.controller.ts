76c971f2b30e1481c5c4c63e4a6aded4984dedf0
thomegabi <gabi10thome@hotmail.com>
2024-05-27 22:40:18 -0300

    Ajustes finais  nos caminhos HTTP

--- src/infra/http/controlers/notifications.controller.ts
+++ src/infra/http/controlers/notifications.controller.ts
@@ -1,11 +1,70 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { CreateNotificationBody } from '../dtos/create-notification-body';
 import { SendNotification } from '@application/use-cases/send-notification';
 import { NotificationViewModel } from '../dtos/view-models/notification-view-model';
+import { CancelNotification } from '@application/use-cases/cancel-notification';
+import { ReadNotification } from '@application/use-cases/read-notification';
+import { UnreadNotification } from '@application/use-cases/unread-notification';
+import { CountRecipientNotification } from '@application/use-cases/count-recipient-notifications';
+import { GetRecipientNotification } from '@application/use-cases/get-recipient-notifications';
 
 @Controller("notifications") // Se for colocado algo dentro desse decorator como "app", vai ser necessário expecificar na URL final, nesse caso seris "localhost:3000/app" 
 export class NotificationsController {
-  constructor(private sendNotification: SendNotification){}
+  constructor(
+    private sendNotification: SendNotification,
+    private cancelNotifcation: CancelNotification,
+    private readNotification: ReadNotification,
+    private unreadNotification: UnreadNotification,
+    private countRecipientNotifications: CountRecipientNotification,
+    private getRecipientNotifications: GetRecipientNotification,
+  ){}
+
+
+@Patch(':id/cancel') // Decorator que atualiza um dado especifico com o caminho expecificado tambem
+async cancel(@Param('id') id: string){ // o decorator Param serve para identar uma variavel. passe o nome e a qual variavel ele se dirige
+  await this.cancelNotifcation.execute({
+    notificationID: id,
+  })
+}
+
+@Get('count/from/:recipientID')
+async countFromRecipient(
+  @Param('recipientID')recipientID: string,
+): Promise<{count:number}> { // É uma boa pratica declarar o retorno quando o método possuir retorno, para evitar erros
+    const {count} = await this.countRecipientNotifications.execute({
+     recipientID,
+    });
+
+    return{
+      count,
+    }
+}
+
+@Get('from/:recipientID')
+async getFromRecipient(
+  @Param('recipientID')recipientID: string) { // É uma boa pratica declarar o retorno quando o método possuir retorno, para evitar erros
+    const {notifications} = await this.getRecipientNotifications.execute({
+     recipientID,
+    });
+
+    return{
+      notifications: notifications.map(NotificationViewModel.toHTTP),
+    }
+}
+
+@Patch(':id/read') // Decorator que atualiza um dado especifico com o caminho expecificado tambem
+async read(@Param('id') id: string){ // o decorator Param serve para identar uma variavel. passe o nome e a qual variavel ele se dirige
+  await this.readNotification.execute({
+    notificationID: id,
+  })
+}
+
+@Patch(':id/unread') // Decorator que atualiza um dado especifico com o caminho expecificado tambem
+async unread(@Param('id') id: string){ // o decorator Param serve para identar uma variavel. passe o nome e a qual variavel ele se dirige
+  await this.unreadNotification.execute({
+    notificationID: id,
+  })
+}
 
 @Post() // Agora com o post ele vai postar informações, ou seja, enviar notificações e não pegalas a penas cono o @Get
   async create(@Body() body: CreateNotificationBody) {
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -2,11 +2,23 @@ import { Module } from "@nestjs/common";
 import { NotificationsController } from "./controlers/notifications.controller";
 import { SendNotification } from "@application/use-cases/send-notification";
 import { DatabaseModule } from "../database/database.module";
+import { CancelNotification } from "@application/use-cases/cancel-notification";
+import { GetRecipientNotification } from "@application/use-cases/get-recipient-notifications";
+import { CountRecipientNotification } from "@application/use-cases/count-recipient-notifications";
+import { ReadNotification } from "@application/use-cases/read-notification";
+import { UnreadNotification } from "@application/use-cases/unread-notification";
 
 @Module({
     imports: [DatabaseModule],
     controllers: [NotificationsController],
-    providers: [SendNotification],
+    providers: [
+        SendNotification,
+        CancelNotification,
+        GetRecipientNotification,
+        CountRecipientNotification, 
+        ReadNotification,
+        UnreadNotification,
+    ],
 })
 
 export class HttpModule {}
